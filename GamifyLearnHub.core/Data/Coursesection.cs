15e96edafe40535375178d14814043d2addf49c0
Mones Makahleh <mones989@hotmail.com>
2023-12-07 20:59:19 +0300

    Scaffold Command and generate Models

---
+++ GamifyLearnHub.core/Data/Coursesection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GamifyLearnHub.Core.Data
+{
+    public partial class Coursesection
+    {
+        public Coursesection()
+        {
+            Lectures = new HashSet<Lecture>();
+        }
+
+        public decimal Coursesectionid { get; set; }
+        public decimal? Courseid { get; set; }
+        public string Coursesectionname { get; set; } = null!;
+        public decimal Coursesectionduration { get; set; }
+
+        public virtual Course? Course { get; set; }
+        public virtual ICollection<Lecture> Lectures { get; set; }
+    }
+}
