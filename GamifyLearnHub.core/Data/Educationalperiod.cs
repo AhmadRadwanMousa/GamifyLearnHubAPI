c537193f0ef4f49e1de265bd68aa1faec79855e4
mohamad foudeh <mohamadfoudeh19@gmail.com>
2023-12-10 00:12:35 +0300

    #error Scafold

--- GamifyLearnHub.core/Data/Educationalperiod.cs
+++ GamifyLearnHub.core/Data/Educationalperiod.cs
@@ -7,16 +7,13 @@ namespace GamifyLearnHub.Core.Data
     {
         public Educationalperiod()
         {
-            Coursesequences = new HashSet<Coursesequence>();
             Programs = new HashSet<Program>();
         }
 
         public decimal Educationalperiodid { get; set; }
         public DateTime Startdate { get; set; }
         public DateTime Enddate { get; set; }
-        public DateTime? Startdatebystamp { get; set; }
 
-        public virtual ICollection<Coursesequence> Coursesequences { get; set; }
         public virtual ICollection<Program> Programs { get; set; }
     }
 }
