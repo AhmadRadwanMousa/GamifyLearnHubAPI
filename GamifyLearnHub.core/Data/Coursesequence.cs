c09cac12038acb4b7604d9eb7bbfcb3e1bdcf771
AHMAD MOUSA <armousa19@cit.just.edu.jo>
2023-12-30 20:02:04 +0300

    commit

--- GamifyLearnHub.core/Data/Coursesequence.cs
+++ GamifyLearnHub.core/Data/Coursesequence.cs
@@ -7,7 +7,7 @@ namespace GamifyLearnHub.Core.Data
     {
         public Coursesequence()
         {
-            CertificationsNavigation = new HashSet<Certification>();
+            Certifications = new HashSet<Certification>();
             Sections = new HashSet<Section>();
         }
 
@@ -15,14 +15,13 @@ namespace GamifyLearnHub.Core.Data
         public decimal? Courseid { get; set; }
         public decimal? Programid { get; set; }
         public decimal? Perviouscourseid { get; set; }
-        public DateTime? Startdate { get; set; }
+        public DateTime Startdate { get; set; }
         public DateTime? Enddate { get; set; }
-        public decimal? Certifications { get; set; }
 
         public virtual Course? Course { get; set; }
         public virtual Course? Perviouscourse { get; set; }
         public virtual Program? Program { get; set; }
-        public virtual ICollection<Certification> CertificationsNavigation { get; set; }
+        public virtual ICollection<Certification> Certifications { get; set; }
         public virtual ICollection<Section> Sections { get; set; }
     }
 }
--- GamifyLearnHub.core/Data/Section.cs
+++ GamifyLearnHub.core/Data/Section.cs
@@ -7,7 +7,6 @@ namespace GamifyLearnHub.Core.Data
     {
         public Section()
         {
-            Announcements = new HashSet<Announcement>();
             Assignments = new HashSet<Assignment>();
             Attendences = new HashSet<Attendence>();
             Coursesections = new HashSet<Coursesection>();
@@ -25,7 +24,6 @@ namespace GamifyLearnHub.Core.Data
 
         public virtual Coursesequence? Coursesequence { get; set; }
         public virtual User? User { get; set; }
-        public virtual ICollection<Announcement> Announcements { get; set; }
         public virtual ICollection<Assignment> Assignments { get; set; }
         public virtual ICollection<Attendence> Attendences { get; set; }
         public virtual ICollection<Coursesection> Coursesections { get; set; }
