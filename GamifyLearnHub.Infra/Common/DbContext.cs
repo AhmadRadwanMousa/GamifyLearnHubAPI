fa2e6fa64d2fd77b5353204439cbc4e2a7fb9c89
AHMAD MOUSA <armousa19@cit.just.edu.jo>
2024-01-02 12:27:32 +0300

    commit

--- GamifyLearnHub.Infra/Common/DbContext.cs
+++ GamifyLearnHub.Infra/Common/DbContext.cs
@@ -13,27 +13,51 @@ namespace GamifyLearnHub.Infra.Common
 {
     public class DbContext : IDbContext
     {
-        private  DbConnection _connection;  
+        private readonly  DbConnection _connection;  
         private readonly IConfiguration _configuration;
+        private bool _disposed = false;
         public DbContext(IConfiguration configuration)
         {
             _configuration=configuration;
+            _connection = new OracleConnection(_configuration.GetConnectionString("DbConnectionString"));
+            _connection.Open();
         }
 
         public DbConnection Connection
         {
             get
             {
-                if (_connection == null) {
-                    _connection = new OracleConnection(_configuration.GetConnectionString("DbConnectionString"));
-                    _connection.Open();
-                }
-                else if (_connection.State!=ConnectionState.Open)
+              
+                if (_connection.State!=ConnectionState.Open)
                 {
                     _connection.Open();
                 }
                 return _connection; 
             }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    if (_connection != null)
+                    {
+                        _connection.Close();
+                        _connection.Dispose();
+                    }
+                }
+                _disposed = true;
+            }
+        }
+        ~DbContext()
+        {
+            Dispose(false);
         }
     }
 }
