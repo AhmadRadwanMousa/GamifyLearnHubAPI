cccf6615b360657f725ec694adafa567f4e0ae52
Mones Makahleh <mones989@hotmail.com>
2024-01-08 21:42:40 +0300

    ..

--- GamifyLearnHub.core/Data/Userlogin.cs
+++ GamifyLearnHub.core/Data/Userlogin.cs
@@ -6,16 +6,16 @@ namespace GamifyLearnHub.Core.Data
     public partial class Userlogin
     {
         public decimal Userloginid { get; set; }
-        public decimal? Userid { get; set; }
+        public decimal Userid { get; set; }
         public string Username { get; set; } = null!;
-        public string Password { get; set; } = null!;
         public decimal? Roleid { get; set; }
         public bool? Isonline { get; set; }
         public bool? Isaccepted { get; set; }
-        public DateTime Lastlogin { get; set; }
+        public string Password { get; set; } = null!;
+        public DateTime? Lastlogin { get; set; }
         public decimal? Dayscount { get; set; }
 
         public virtual Role? Role { get; set; }
-        public virtual User? User { get; set; }
+        public virtual User User { get; set; } = null!;
     }
 }
